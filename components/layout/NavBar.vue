a85ec441ad2151f082aa217b9cf3191d4b19728b
mazentarek88 <mazentarek668@gmail.com>
2025-07-11 23:16:23 +0300

    hi

--- components/layout/NavBar.vue
+++ components/layout/NavBar.vue
@@ -1,184 +1,92 @@
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from "vue";
-import { useCookie } from "#imports";
-import arweqaLogo from "@/assets/images/logos/arweqa-logo.svg";
-import saudiFlag from "@/assets/images/logos/saudi-flag.svg";
-import unitedkingdomFlag from "@/assets/images/logos/united-kingdom-flag.svg";
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
 
-const route = useRoute();
-
-// Detecting active section and changing navbar depending on scrolling
-const isScrolled = ref(false);
-onMounted(() => {
-  isScrolled.value = window.scrollY > 50;
-});
-
-interface Section {
-  url: string;
-  name: string;
-}
-const sections: Section[] = [
-  { url: "home", name: "Home" },
-  { url: "demo", name: "Demo" },
-];
-
-const handleScroll = () => {
-  isScrolled.value = window.scrollY > 50;
-};
+const showNavbar = ref(false);
+const router = useRouter();
 
 onMounted(() => {
-  window.addEventListener("scroll", handleScroll);
-  handleScroll();
+  setTimeout(() => {
+    showNavbar.value = true;
+  }, 300);
 });
-
-onUnmounted(() => {
-  window.removeEventListener("scroll", handleScroll);
-});
-
-// Language handling
-const pickedLanguage = useCookie("preferred-language");
-function changeLanguage() {
-  pickedLanguage.value = pickedLanguage.value === "ar" ? "en" : "ar";
-
-  const toRoute = route.path.includes("/ar/")
-    ? route.path.replace("/ar/", "/en/")
-    : route.path.replace("/en/", "/ar/");
-
-  location.pathname = toRoute;
-}
 </script>
 
 <template>
-  <nav
-    class="active"
-    :class="{
-      'bg-[#efb81a]': !isScrolled,
-      'bg-black': !!isScrolled,
-    }"
-  >
-    <div class="container nav-container">
-      <div class="nav-links">
-        <div class="img-con">
-          <img :src="arweqaLogo" alt="logo" @click="$router.push(`/home`)" />
-        </div>
-        <div class="router-links">
-          <ul>
-            <!-- Links -->
-            <li v-for="section in sections" :key="section.id">
-              <NuxtLink :to="{ name: section.url }">
-                <span class="text-white">
-                  {{ section.name }}
-                </span>
-              </NuxtLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+  <nav :class="['navbar', showNavbar ? 'show' : '']">
+    <ul class="nav-left">
+      <li><NuxtLink to="/">Home</NuxtLink></li>
+      <li><NuxtLink to="/products">Catalog</NuxtLink></li>
+      <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+    </ul>
+    <div class="logo">STↃTELY</div>
+    <ul class="nav-right">
+      <li>
+        <a href="#"><span class="material-symbols-outlined">search</span></a>
+      </li>
+      <li>
+        <a href="#"><span class="material-symbols-outlined">person</span></a>
+      </li>
+      <li>
+        <a href="#"
+          ><span class="material-symbols-outlined">shopping_bag</span></a
+        >
+      </li>
+    </ul>
   </nav>
 </template>
 
-<style lang="scss" scoped>
-nav {
-  position: sticky;
+<style scoped>
+@font-face {
+  font-family: "Brother Signature";
+  src: url("assets/fonts/BrotherSignature-7BWnK.otf") format("opentype");
+  font-weight: normal;
+  font-style: normal;
+}
+
+.navbar {
+  opacity: 0;
+  transform: translateY(-20px);
+  transition: all 0.8s ease;
+  position: absolute;
   top: 0;
-  z-index: 50;
-  box-shadow: 0px 4px 6px -1px #0a0d120f;
   width: 100%;
+  padding: 1px 8px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 80px;
-  padding: 17px 0;
-  .nav-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    gap: 16px;
-    position: relative;
-    padding-top: 17px;
-    padding-bottom: 17px;
+  color: white;
+  z-index: 2;
+}
 
-    .nav-links {
-      display: flex;
-      justify-content: center;
-      gap: 16px;
-      .img-con {
-        width: 120px;
-        height: 37px;
-        cursor: pointer;
+.navbar.show {
+  opacity: 1;
+  transform: translateY(0);
+}
 
-        @media (max-width: 768px) {
-          width: 100px;
-          height: 32px;
-          margin-top: 5px;
-        }
+.navbar ul {
+  list-style: none;
+  display: flex;
+  gap: 20px;
+  padding: 0;
+  margin: 0;
+}
 
-        img {
-          width: 100%;
-          height: 100%;
-          object-fit: cover;
-        }
-      }
-      .router-links {
-        ul {
-          display: flex;
-          gap: 16px;
+.navbar a {
+  text-decoration: none;
+  color: white;
+  font-weight: 500;
+  font-size: 12px;
+  font-style: italic;
+  transition: color 0.3s ease;
+}
 
-          @media (max-width: 768px) {
-            gap: 4px;
-          }
-          li {
-            color: #181d27;
-            font-weight: 500;
-            white-space: nowrap;
-            line-height: 28px;
-            padding: 8px 7px;
-            .router-link-active {
-              span {
-                cursor: pointer;
-                &::after {
-                  content: "";
-                  background-color: white;
-                  margin: 0 auto;
-                  width: calc(100% - 11px);
-                  height: 4px;
-                  border-style: solid;
-                  border-width: 0px;
-                  border-radius: 200px 200px 0px 0px;
-                  margin-top: 3px;
-                  display: block;
-                }
-              }
-            }
-            &.lang {
-              display: flex;
-              padding-top: 2px;
-              gap: 4px;
-              align-items: center;
-              cursor: pointer;
-              .image-con {
-                width: 26px;
-                height: 20px;
-                border-radius: 4px;
-                cursor: pointer;
-                img {
-                  width: 100%;
-                  height: 100%;
-                  object-fit: cover;
-                }
-              }
-              span {
-                font-size: 14px;
-                line-height: 20px;
-                font-weight: 500;
-                color: white;
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+.navbar a:hover {
+  color: black;
+}
+
+.logo {
+  font-family: "Brother Signature", cursive;
+  font-size: 24px;
 }
 </style>
